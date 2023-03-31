e35ed81ffd7634baf1903c58f48ccf9c46a5e44c
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2023-03-31 12:27:14 +0530

    Added cifar100 evolution_augment config

--- evolution_augment/run.py
+++ evolution_augment/run.py
@@ -2,7 +2,6 @@ import os
 import time
 from datetime import datetime
 
-from torch.nn import CrossEntropyLoss
 import wandb
 
 from mlproject.boilerplate import Learner
@@ -13,13 +12,15 @@ from evolution_augment import config
 from evolution_augment import dataloaders
 from evolution_augment import models
 from evolution_augment import optimizers
+from evolution_augment import trainers
+from evolution_augment import transforms
 
 if __name__ == '__main__':
-    experiment_cfg = config.wideresnet28x18_randaugment_cifar100_bs256_ep200()
+    experiment_cfg = config.wideresnet28x18_evoaugment_cifar100_bs256_ep200_n25()
 
     os.makedirs(experiment_cfg.experiment_dir, exist_ok=True)
     wandb.init(
-        name='wideresnet28x18_randaugment_cifar100_bs256_ep200-[{}]'.format(
+        name='wideresnet28x18_evoaugment_cifar100_bs256_ep200_n25-[{}]'.format(
             datetime.now().strftime('%d-%m-%Y|%H:%M:%S')),
         project=os.environ.get('WANDB_PROJECT',
                                experiment_cfg.train_dataloader.dataset.name),
@@ -38,7 +39,28 @@ if __name__ == '__main__':
         experiment_cfg.train_dataloader)
     val_dataloader = dataloaders.DataloaderFactory.build(
         experiment_cfg.val_dataloaders)
-    criterion = CrossEntropyLoss()
+
+    if experiment_cfg.train_dataloader.augmentation.evolution_augment:
+        evo_aug_cfg = experiment_cfg.train_dataloader.augmentation.evolution_augment
+        evolution_augment = transforms.EvolutionAugmentV2(
+            num_candidates=evo_aug_cfg.num_candidates,
+            num_ops=evo_aug_cfg.num_ops,
+            magnitude=evo_aug_cfg.magnitude,
+            cutout_length=0,
+            mean=experiment_cfg.train_dataloader.dataset.image_means,
+            std=experiment_cfg.train_dataloader.dataset.image_stds)
+
+        trainer = trainers.EvolutionAugmentTrainer(
+            evolution_augment=evolution_augment,
+            optimizer=optimizer,
+            scheduler=lr_sched,
+            experiment_tracker=wandb)
+
+    else:
+        trainer = ClassificationTrainer(
+            optimizer=optimizer,
+            scheduler=lr_sched,
+            experiment_tracker=wandb)
 
     experiment = Learner(
         experiment_name=experiment_cfg.experiment_name,
@@ -46,13 +68,9 @@ if __name__ == '__main__':
         model=model,
         train_dataloader=train_dataloader,
         val_dataloaders=[val_dataloader],
-        trainers=[
-            ClassificationTrainer(optimizer=optimizer,
-                                  scheduler=lr_sched,
-                                  experiment_tracker=wandb)
-        ],
+        trainers=[trainer],
         evaluators=[ClassificationEvaluator(experiment_tracker=wandb)],
-        evaluate_every_n_steps=500,
+        evaluate_every_n_steps=250,
         checkpoint_every_n_steps=experiment_cfg.train_iters // 10,
         checkpoint_after_validation=True,
         train_iters=experiment_cfg.train_iters,
