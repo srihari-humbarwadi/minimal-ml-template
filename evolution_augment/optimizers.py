a9e0ac4d8386fd144b8906daaf98c1a394724ebc
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2023-04-09 05:43:14 +0000

    Exclude bias and batchnorm variables from weight_decay

--- evolution_augment/optimizers.py
+++ evolution_augment/optimizers.py
@@ -3,8 +3,26 @@ import dataclasses
 import torch
 from transformers import optimization
 
+def _add_weight_decay(params, weight_decay=1e-5):
+    '''
+    reference: https://github.com/huggingface/pytorch-image-models/blob/1bb3989b61c083f89a362f6d1f2c60af35c36203/timm/optim/optim_factory.py#L45
+    '''
+    decay = []
+    no_decay = []
+    for param in params:
+        if not param.requires_grad:
+            continue
+        if len(param.shape) == 1:
+            no_decay.append(param)
+        else:
+            decay.append(param)
+    return [
+        {'params': no_decay, 'weight_decay': 0.},
+        {'params': decay, 'weight_decay': weight_decay}]
+
 
 def get_optimizer(params, config):
+    params = _add_weight_decay(params, weight_decay=config.weight_decay)
     if config.__class__.__name__ == 'SGD':
         config = dataclasses.asdict(config)
         print('Building SGD optimizer with config:\n{}'.format(config))
